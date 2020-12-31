616cd387ae88228980be0f523776b7ee9e0f1afa
mr904i <hashimoto_keisuke@hashi-MBP.local>
2020-12-31 18:20:37 +0900

    add column

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -2,11 +2,25 @@
 class AdminsController < ApplicationController
   def index; end
 
-  def new; end
+  def new
+    @feed = Feed.new
+  end
 
   def create
-    puts 'we can insert scraping text'
+    text = @feed.save_node(url: feed_params[:url], node: feed_params[:node], scrape_type: feed_params[:scrape_type])
+    # todo adminsにも入力情報を保存する
+    @feed.create(text: text) if text.present?
+
+    if @feed.save
+      redirect_to action: :index
+    else
+      render :new
+    end
   end
 
   private
+
+  def feed_params
+    params.require(:admin).permit(:url, :node, :scrape_type)
+  end
 end
--- app/models/feed.rb
+++ app/models/feed.rb
@@ -1,2 +1,14 @@
 class Feed < ApplicationRecord
+  belongs_to :admin
+
+  def save_node(url, node, scrape_type)
+    text ||= scraping_execute(url, node, scrape_type)
+  end
+
+  private
+
+  def scraping_execute(url, node, scrape_type)
+    Scrape::NokogiriService.new(url: url, node: node, scrape_type: scrape_type)
+                           .execute
+  end
 end
